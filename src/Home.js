8d80d8ea89e88b3437b26ed5f3ad4d4baf394b2e
Humberto M <humbertomonterrosa@gmail.com>
2022-01-03 15:58:52 -0500

    Custom base path react js

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import { Link } from 'react-router-dom';
+function Home() {
+  return (
+   <div>
+     <h1>Home</h1>
+     <img src={logo} width={200} />
+     <Link to='/other'>Other Page</Link>
+    </div>
+  );
+}
+
+export default Home;
